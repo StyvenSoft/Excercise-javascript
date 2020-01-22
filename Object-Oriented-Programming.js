d732c42e8fbd5dd0e04e5514b741e14c895086ae
Steveen <steveen.echeverri@gmail.com>
2020-01-21 21:45:20 -0500

    Basic POO exercise

---
+++ Object-Oriented-Programming.js
@@ -0,0 +1,106 @@
+// Creacion de objetos simples
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+
+let dog = {
+    name : "Ruff",
+    numLegs : 4
+};
+let cat = {
+  name: "Spot",
+  numLegs: 4
+};
+console.log(cat.name);
+console.log(cat.numLegs);
+
+// creando un método en un objeto
+
+let hen = {
+  name: "Lhit",
+  numLegs: 2,
+  sayName: function() {return "The name of this hen is " + hen.name + ".";}
+};
+console.log(hen.sayName());
+
+let parrot = {
+  name: "Spot",
+  numLegs: 2,
+  sayLegs : function (){ return "This parrot has " + parrot.numLegs + " legs."  }
+};
+
+console.log(parrot.sayLegs());
+
+// reutilizar objeto con la palabra clave "This"
+
+let pigeon = {
+  name: "Frehh",
+  numLegs: 2,
+  sayName: function() {return "The name of this pigeon is " + this.name + ".";}
+};
+console.log(pigeon.sayName())
+let horse = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {return "This horse has " + this.numLegs + " legs.";}
+};
+
+console.log(horse.sayLegs());
+
+// Definir una Función Constructora
+// Los constructores son funciones que crean nuevos objetos
+
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+}
+let blueBird = new Bird();
+
+blueBird.name; // => Albert
+blueBird.color; // => blue
+blueBird.numLegs; // => 2
+// Modificando su propiedad sayName
+blueBird.name = 'Elvira';
+blueBird.name; // => Elvira
+
+
+// se definen con un nombre en mayúscula
+// usan la palabra clave this para establecer las propiedades del objeto que crearán
+// definen propiedades y comportamientos en lugar de devolver un valor como podrían hacerlo otras funciones
+
+function Dog (){
+    this.name = "Yiko";
+    this.color = "Brown";
+    this.numLegs = 4
+}
+
+let hound = new Dog();
+
+// ampliando los constructores para recibir argumentos
+function BirdTwo(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+
+// Pasando los valores como argumentos
+let cardinal = new Bird("Bruce", "red");
+
+cardinal.name // => Bruce
+cardinal.color // => red
+cardinal.numLegs // => 2
+
+console.log(cardinal);
+
+function DogTwo(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+}
+
+let terrier = new DogTwo("Aldre", "Black");
+
+console.log(terrier);
