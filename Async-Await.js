3daeb0113e890420f0e13e4d2961ddfc49a02038
Steveen <steveen.echeverri@gmail.com>
2020-03-03 19:07:55 -0500

    Add functions Async

---
+++ Async-Await.js
@@ -0,0 +1,91 @@
+// Manejar acciones asincrónicas async...await
+
+// mejora poderosamente la legibilidad y escalabilidad de nuestro código.
+
+const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
+
+// Here we use fs.readfile() and callback functions:
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  let firstSentence = data;
+  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
+    if (err) throw err;
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  });
+});
+
+// Here we use native promises with our "promisified" version of readfile:
+let firstSentence
+promisifiedReadfile('./file.txt', 'utf-8')
+  .then((data) => {
+    firstSentence = data;
+    return promisifiedReadfile('./file2.txt', 'utf-8')
+  })
+  .then((data) => {
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  })
+  .catch((err) => {console.log(err)});
+
+// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8')
+  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8')
+  console.log(firstSentence, secondSentence)
+}
+readFiles()
+
+// La palabra clave "async"
+// podemos crear asyncexpresiones de funciones
+
+const myFunc = async () => {
+  // Function body here
+};
+
+myFunc();
+
+// Las funciones "async" siempre devuelven una promesa
+
+async function fivePromise() {
+  return 5;
+}
+
+fivePromise()
+.then(resolvedValue => {
+    console.log(resolvedValue);
+  })  // Prints 5
+
+
+//Ejemplo funcion
+
+function withConstructor(num){
+  return new Promise((resolve, reject) => {
+    if (num === 0){
+      resolve('zero');
+    } else {
+      resolve('not zero');
+    }
+  })
+}
+
+withConstructor(0)
+  .then((resolveValue) => {
+  console.log(` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`);
+})
+
+// Ejemplo Funcion Async:
+
+async function withAsync(num){
+  if (num === 0){
+      return 'zero';
+    } else {
+      return 'not zero';
+    }
+}
+
+withAsync(100)
+  .then((resolveValue) => {
+  console.log(` withAsync(100) returned a promise which resolved to: ${resolveValue}.`);
+})
