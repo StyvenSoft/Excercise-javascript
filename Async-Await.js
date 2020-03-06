7c1f0e509f2bffdf3ad16d2d784c480439c3a327
Steveen <steveen.echeverri@gmail.com>
2020-03-05 20:57:28 -0500

    Dependent Promise and Erros

--- Async-Await.js
+++ Async-Await.js
@@ -157,3 +157,83 @@ async function getBeans() {
 }
 
 getBeans();
+
+
+// Manejo de promesas dependientes
+// sintaxis nativa de promesas
+
+function nativePromiseVersion() {
+    returnsFirstPromise()
+    .then((firstValue) => {
+        console.log(firstValue);
+        return returnsSecondPromise(firstValue);
+    })
+   .then((secondValue) => {
+        console.log(secondValue);
+    });
+}
+
+// asyncfunción para lograr lo mismo
+
+async function asyncAwaitVersion() {
+ let firstValue = await returnsFirstPromise();
+ console.log(firstValue);
+ let secondValue = await returnsSecondPromise(firstValue);
+ console.log(secondValue);
+}
+
+// Example:
+
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+async function makeBeans() {
+  let type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+  console.log(dinner);
+}
+
+makeBeans();
+
+// Errores de manejo
+
+// Con async...await, utilizamos try...catch
+
+async function usingTryCatch() {
+ try {
+   let resolveValue = await asyncFunction('thing that will fail');
+   let secondValue = await secondAsyncFunction(resolveValue);
+ } catch (err) {
+   // Catches any errors in the try block
+   console.log(err);
+ }
+}
+
+usingTryCatch();
+
+// promesas nativas .catch()con una asyncfunción
+
+async function usingPromiseCatch() {
+   let resolveValue = await asyncFunction('thing that will fail');
+}
+
+let rejectedPromise = usingPromiseCatch();
+rejectedPromise.catch((rejectValue) => {
+console.log(rejectValue);
+})
+
+// Example 
+
+const cookBeanSouffle = require('./library.js');
+
+async function hostDinnerParty(){
+  try{
+    let dinner = await cookBeanSouffle();
+     console.log(`${dinner} is served!`);
+  }catch(error) {
+    console.log(error);
+console.log('Ordering a pizza!');
+  }
+}
+
+hostDinnerParty();
