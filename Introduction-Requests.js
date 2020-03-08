6026848df144d03e71afb16614b4a27cd66215c2
Steveen <steveen.echeverri@gmail.com>
2020-03-08 12:53:01 -0500

    Requests AJAX POST complete

--- Introduction-Requests.js
+++ Introduction-Requests.js
@@ -107,3 +107,20 @@ const displaySuggestions = (event) => {
 }
 
 submit.addEventListener('click', displaySuggestions);
+
+// solicitud AJAX POST completa
+
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+
+xhr.responseType = 'json';
+
+xhr.onreadystatechange = () =>{
+  if(xhr.readyState === XMLHttpRequest.DONE){
+    return xhr.response;
+  }
+}
+
+xhr.open('POST', url);
+xhr.send(data);
