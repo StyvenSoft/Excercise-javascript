b8b7013b92d2cd141f85c418f893d1cf921c141d
Steveen <steveen.echeverri@gmail.com>
2020-03-09 12:25:19 -0500

    Examples fetch() Get Resquests

--- Introduction-Requests.js
+++ Introduction-Requests.js
@@ -165,3 +165,109 @@ const displayShortUrl = (event) => {
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
+
+
+// Example JSON Jenerator
+
+
+const jsonButton = document.querySelector('#generate');
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#displayContainer');
+const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click me", "A new one"];
+
+const generateJson = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+      changeButton();
+    }
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+const formatJson = (resJson) => {
+  resJson = JSON.stringify(resJson);
+  let counter = 0;
+  return resJson.split('')
+  .map(char => {
+    switch (char) {
+      case ',':
+        return `,\n${' '.repeat(counter * 2)}`;
+      case '{':
+        counter += 1;
+        return `{\n${' '.repeat(counter * 2)}`;
+      case '}':
+        counter -= 1;
+        return `\n${' '.repeat(counter * 2)}}`;
+      default:
+        return char;
+    }
+  })
+  .join('');
+};
+
+const renderResponse = (jsonResponse) => {
+  const jsonSelection = Math.floor(Math.random() * 10);
+  display.innerHTML = `<pre>${formatJson(jsonResponse[jsonSelection])}</pre>`;
+};
+
+const changeButton = () => {
+  const newText = Math.floor(Math.random() * 7);
+  jsonButton.innerHTML = `${collection[newText]}!`;
+};
+
+jsonButton.addEventListener('click', generateJson);
+
+
+// fetch() GET Requests
+//Las solicitudes GET que usan fetch()
+
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
+
+// Acceder a la API de Datamuse y generar información en el navegador.
+// fetch() GET Requests
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  fetch(endpoint, {cache: 'no-cache'}).then(response =>{
+    if(response.ok){
+       return response.json()
+    }
+    throw new Error('Request failed!');
+  },(networkError) =>{
+    console.log(networkError.message);
+  });
+}
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
