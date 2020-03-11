df6eefb8aa1adb652a24341af0b1a968e9662900
Steveen <steveen.echeverri@gmail.com>
2020-03-11 11:23:00 -0500

    Example Complete fetch() POST requests

--- Introduction-Requests.js
+++ Introduction-Requests.js
@@ -271,3 +271,103 @@ const displaySuggestions = (event) => {
 };
 
 submit.addEventListener('click', displaySuggestions);
+
+// Tomar la información que se le devolvió con la promesa y manipulará la página web.
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+
+// Selects page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+
+  fetch(endpoint, {cache: 'no-cache'}).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then((jsonResponse) =>{
+    renderResponse(jsonResponse);
+  })
+}
+
+// Clears previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
+// fetch() POST Requests
+
+// solicitud POST completa usando fetch()y .then().
+
+fetch('https://api-to-call.com/endpoint', {
+  method : 'POST',
+  body : JSON.stringify({id: '200'})
+}).then(response => {
+  if(response.ok){
+     return response.json();
+  } throw new Error('Request failed!');
+}, networkError => console.log(networkError.message)
+).then(jsonResponse => {
+    // code the execute with JsonResponse
+  }
+);
+
+// fetch() Post Requests
+
+// acortar una URL utilizando la API de acortamiento de URL de Rebrandly.
+
+
+// Information to reach API
+const apiKey = '478973e548b0487bab04107b53ba757c';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  fetch(url, { method : 'POST', headers : {
+  'Content-type': 'application/json',
+  'apikey': apiKey
+    },
+    body : data
+  }).then(response =>{
+    if(response.ok){
+       return response.json();
+    } throw new Error ('Request failed!');
+  }, (networkError) => console.log(networkError.message)
+  ).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  });
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
