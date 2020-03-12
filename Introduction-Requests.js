f7bac229ca19b71617449cf73d9bf250baaa223d
Steveen <steveen.echeverri@gmail.com>
2020-03-12 17:08:17 -0500

    Complete example async POST requests

--- Introduction-Requests.js
+++ Introduction-Requests.js
@@ -430,3 +430,70 @@ const displaySuggestions = (event) => {
 }
 
 submit.addEventListener('click', displaySuggestions);
+
+// async POST Requests
+
+//solicitud POST usando asyncy await.
+
+const getData = async () => {
+  try{
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method : 'POST',
+      body : JSON.stringify({id: 200})
+    })
+    if(response.ok) {
+     const jsonResponse = await response.json();
+      return jsonResponse;
+     } throw new Error('Request failed!');
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+// async POST Requests
+
+// recuperar su clave API Rebrandly para acceder a la API
+
+// information to reach API
+const apiKey = '478973e548b0487bab04107b53ba757c';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+// Code goes here
+const shortenUrl = async () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+  try {
+    const response = await fetch(url, {
+      method : 'POST',
+      body : data,
+      headers : {
+        'Content-type': 'application/json',
+        'apikey': apiKey
+      }
+    })
+    if(response.ok) {
+       const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
