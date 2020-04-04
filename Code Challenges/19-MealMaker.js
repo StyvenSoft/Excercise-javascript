c170a033dbd8ad4b72cadfb5f9aa75d0c86ef546
Steveen <steveen.echeverri@gmail.com>
2020-04-04 12:18:09 -0500

    Complete exercise MealMaker

--- Code Challenges/19-MealMaker.js
+++ Code Challenges/19-MealMaker.js
@@ -4,13 +4,7 @@ const menu = {
       mains : [], 
       desserts : []
     },
-    get courses() {
-      return {
-        appetizers: this.appetizers,
-        mains: this.mains,
-        desserts: this.desserts
-      }
-    },
+
     get appetizers() {
       return this._courses.appetizers;
     },
@@ -21,23 +15,57 @@ const menu = {
       return this._courses.mains;
     },
 
-
-    set appetizers(appetizerIn) {
-      
+    set appetizers(appetizers) {
+      this._courses.appetizers = appetizers;
     },
     
-    set mains(appetizerIn) {
-      
+    set mains(mains) {
+      this._courses.mains = mains;
     },
    
-    set desserts(appetizerIn) {
-      
+    set desserts(desserts) {
+      this._courses.desserts = desserts;
     },
-    .addDishToCourse(courseName, dishName, dishPrice) {
+    get courses() {
+        return {
+          appetizers: this.appetizers,
+          mains: this.mains,
+          desserts: this.desserts
+        }
+      },
+    addDishToCourse(courseName, dishName, dishPrice) {
       const dish = {
-       
+        name: dishName,
+        price: dishPrice
       };
   
-      this._courses[courseName].push(dish);
+      return this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse(courseName) {
+        const dishes = this._courses[courseName];
+        const randomIndex = Math.floor(Math.random() * dishes.length);
+        return dishes[randomIndex];
+    },
+    generateRandomMeal() {
+        const appetizer = this.getRandomDishFromCourse('appetizers');
+        const main = this.getRandomDishFromCourse('mains');
+        const dessert = this.getRandomDishFromCourse('desserts');
+        const totalPrice = appetizer.price + main.price + dessert.price;
+        return `Your meal is ${appetizer.name}, ${main.name} and ${dessert.name} and the total price is ${totalPrice}`;
     }
   }
+
+menu.addDishToCourse('appetizers', 'salad', 5.00);
+menu.addDishToCourse('appetizers', 'wings', 6.50);
+menu.addDishToCourse('appetizers', 'salad', 4.00);
+
+menu.addDishToCourse('mains', 'steak', 15.25);
+menu.addDishToCourse('mains', 'salmon', 8.75);
+menu.addDishToCourse('mains', 'tofu', 11.00);
+
+menu.addDishToCourse('desserts', 'ice cream', 4.00);
+menu.addDishToCourse('desserts', 'coffe', 3.00);
+menu.addDishToCourse('desserts', 'cake', 3.25);
+
+const meal = menu.generateRandomMeal();
+console.log(meal);
