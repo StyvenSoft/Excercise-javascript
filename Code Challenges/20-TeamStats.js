ba4bc75a34032e8d6b1e05625f61181c7c8656bc
Steveen <steveen.echeverri@gmail.com>
2020-04-05 22:27:57 -0500

    Finished project Team Stats

--- Code Challenges/20-TeamStats.js
+++ Code Challenges/20-TeamStats.js
@@ -14,6 +14,31 @@ const team = {
       },
     get games() {
         return this._games;
+    },
+    addPlayer(firstName, lastName, age) {
+        let player = {
+            firstName : firstName,
+            lastName : lastName,
+            age : age
+        };
+        this.players.push(player)
+    },
+    addGame(opp, myPts, oppPts) {
+        const game = {
+            opponent : opp,
+            teamPoints : myPts,
+            opponentPoints : oppPts 
+        };
+        this.games.push(game);
     }
   }
-  
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team.players);
+
+
+team.addGame('Titans', 100, 98);
+team.addGame('Tigers', 150, 69);
+team.addGame('Lions', 165, 87);
+console.log(team.games);
