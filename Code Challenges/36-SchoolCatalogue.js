2f829909113e3fa00bb54f81bc2007aa27dc2dae
Steveen <steveen.echeverri@gmail.com>
2020-04-24 15:34:03 -0500

    Complete excersice School Catalogue

--- Code Challenges/36-SchoolCatalogue.js
+++ Code Challenges/36-SchoolCatalogue.js
@@ -1,3 +1,4 @@
+// Create ParentSchool Class
 class School {
     constructor (name, level, numberOfStudents) {
         this._name = name;
@@ -28,10 +29,36 @@ class School {
         return substituteTeacher[rantInt];
     }
 }
+// Create PrimarySchool Class
 class PrimarySchool extends School {
     constructor (name, numberOfStudents, pickupPolicy) {
         super(name, 'primary', numberOfStudents);
         this._pickupPolicy = pickupPolicy;
     }
+    get pickupPolicy () {
+        return this._pickupPolicy;
+    }
+}
+// Create HighSchool Class
+
+class HightSchool extends School {
+    constructor (name, numberOfStudents, sportsTeams) {
+        super(name, 'hight', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+    get sportsTeams() {
+        return this._sportsTeams;
+    }
 }
 
+// Instances of PrimarySchool and HighSchool
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+lorraineHansbury.quickFacts();
+
+const subs = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HightSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams);
