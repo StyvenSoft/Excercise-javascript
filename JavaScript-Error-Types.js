a178c9f32a3c267805e11896226864b16fdba187
Steveen <steveen.echeverri@gmail.com>
2020-05-14 19:18:37 -0500

    Exercise debugging errors

--- JavaScript-Error-Types.js
+++ JavaScript-Error-Types.js
@@ -52,7 +52,7 @@ function isSumBigger(number1, number2, total {
   // Should return false
   console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));
 
-  
+
 
 // Solution:
 
@@ -71,3 +71,74 @@ function isSumBigger(number1, number2, total) {
   
   // Should return false
   console.log('isSumBigger(1, 3, 5) returns: ' + isSumBigger(1, 3, 5));
+
+  // Locating Silent Bugs
+
+  /*
+  Puede encontrar que su código devuelve valores incorrectos de manera consistente sin arrojar ningún error. 
+  La falta de errores arrojados no significa que la lógica de su código sea completamente correcta.
+
+  */
+
+ function capitalizeASingleWord(word) {
+    //  console.log(word);
+      if (word.match(' ')) {
+        console.log(word);
+        return null;
+      }
+      
+      let firstLetter = word.charAt(0);
+      const restOfWord = word.slice(1);
+      
+      firstLetter = firstLetter.toUpperCase();
+      
+      return firstLetter + restOfWord;
+    }
+    
+    // Should return "Hey"
+    console.log("capitalizeASingleWord('hey') returns: " + capitalizeASingleWord('hey'));
+
+
+// Debugging with console.log() 
+
+/*
+1 - Vaya al comienzo del código de mal funcionamiento. Imprima todas las variables iniciales, 
+valores existentes y argumentos utilizando console.log(). Si los valores son lo que espera, pase a 
+la siguiente lógica del código. Si no, ha identificado un error y debe pasar al paso 3.
+
+2 - Después de la siguiente pieza de lógica en su código, agregue console.log()declaraciones para
+ asegurarse de que las variables actualizadas tengan los valores que ahora espera y que el bloque de
+  código se esté ejecutando. Si esa lógica se está ejecutando correctamente, continúe repitiendo este 
+  paso hasta que encuentre que una línea no funciona como se esperaba, luego vaya al paso 3.
+
+3 - Solucione el error identificado y vuelva a ejecutar su código. Si ahora funciona como 
+se esperaba, ¡ha terminado de depurar! De lo contrario, continúe recorriendo su código utilizando
+el paso 2 hasta que lo haga.
+
+*/
+ 
+// Returns the string whose first letter is later in the alphabet. 
+// If both first letters are equal, returns null.
+
+function getLaterFirstLetter(string1, string2) {
+    const firstLetter1 = string1.charAt(0);
+    const firstLetter2 = string2.charAt(0);
+    console.log(firstLetter1);
+    console.log(firstLetter2);
+    if (firstLetter1 === firstLetter2) {
+      return null;
+    } else if (firstLetter1 < firstLetter2) {
+      return string2;
+    } else {
+      return string1;
+    }
+  }
+  
+  // Should return "blueberry"
+  console.log("getLaterFirstLetter('avocado', 'blueberry') returns: " + getLaterFirstLetter('avocado', 'blueberry'));
+  
+  // Should return "zebra"
+  console.log("getLaterFirstLetter('zebra', 'aardvark') returns : " + getLaterFirstLetter('zebra', 'aardvark'));
+  
+  // Should return null
+  console.log("getLaterFirstLetter('astro', 'afar') returns: " + getLaterFirstLetter('astro', 'afar'));
